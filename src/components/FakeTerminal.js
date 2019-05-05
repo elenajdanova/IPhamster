7ed09fbc0c70cb9b13ba68a09a64b1661b91358c
Elena <litishia@ya.ru>
2019-05-05 12:53:44 -0400

    add some logic to terminal

--- src/components/FakeTerminal.js
+++ src/components/FakeTerminal.js
@@ -1,33 +1,65 @@
 import styles from './css/fakeTerminal.css';
 import React from 'react';
 
-class FakeTerminal extends React.Component {
-  constructor(props) {
-   super(props);
-   this.output = React.createRef();
-   this.cmdline = React.createRef();
-   this.prompt = React.createRef();
-   this.state = {text: ''};
-}
+let defaultOpts = {
+  welcome: "Welcome!",
+  host: "example.com",
+  user: "guest",
+  is_root: false,
+  speed: 75
+};
 
-  render(){
-          return (
-              <div className="column" id="terminal">
-                  <div id="output" ref={this.output}>{this.state.text}</div>
-                  <div id="input-line" className="input-line">
-                      <div id="prompt" className="prompt-color"
-                           ref={this.prompt}>
-                      </div>&nbsp;
-                      <div>
-                          <input type="text" id="cmdline" ref={this.cmdline}
-                          autoComplete="off"
-                          autoCorrect="off" autoCapitalize="off"
-                          spellCheck="false" autoFocus/>
-                      </div>
+let i = 0;
+let output = '';
+
+class FakeTerminal extends React.Component {
+    constructor(props) {
+       super(props);
+       this.output = React.createRef();
+       this.cmdline = React.createRef();
+       this.prompt = React.createRef();
+       this.state = {text: ''};
+   }
+
+   componentDidMount(){
+       this.handleTyping();
+   }
+
+   handleTyping(){
+        this.timerID = setInterval(
+          () => this.typer(defaultOpts.welcome), defaultOpts.speed
+        );
+   }
+
+   typer(text) {
+       if ( i < text.length ) {
+           let char = text.charAt(i);
+           output += char;
+           this.setState({text: output});
+           i++;
+       } else {
+          clearInterval(this.timerID);
+       }
+   }
+
+   render(){
+        return (
+          <div className="column" id="terminal">
+              <div id="output" ref={this.output}>{this.state.text}</div>
+              <div id="input-line" className="input-line">
+                  <div id="prompt" className="prompt-color"
+                       ref={this.prompt}>
+                  </div>&nbsp;
+                  <div>
+                      <input type="text" id="cmdline" ref={this.cmdline}
+                      autoComplete="off"
+                      autoCorrect="off" autoCapitalize="off"
+                      spellCheck="false" autoFocus/>
                   </div>
               </div>
-          );
-      }
-  }
+          </div>
+      );
+    }
+}
 
   export default FakeTerminal;
