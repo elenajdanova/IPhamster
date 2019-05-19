7ad0191a455151ad423fdb30d84e3678587f9ab4
Elena <litishia@ya.ru>
2019-05-18 22:16:56 -0400

    add generate IPv4 and IPv6 methods

--- src/services/terminalEngine.js
+++ src/services/terminalEngine.js
@@ -1,6 +1,9 @@
 let cmds = {
     start: {value: 'start', help: 'help on start'},
-    whoami: {value: 'whoami', help:'help on whoami'}
+    whoami: {value: 'whoami', help:'help on whoami'},
+    IPv4: {value: 'IPv4', help:'help on IPv4'},
+    IPv6: {value: 'IPv6', help:'help on IPv6'},
+    both: {value: 'both', help:'help on both'}
 }
 
 export default class TerminalEngine {
@@ -13,6 +16,10 @@ export default class TerminalEngine {
         switch (this.command) {
             case cmds.start.value:
                 return this.start();
+            case cmds.IPv4.value:
+                return this.generateIPv4();
+            case cmds.IPv6.value:
+                return this.generateIPv6();
             default:
                 return this.invalidCMD();
         }
@@ -20,6 +27,40 @@ export default class TerminalEngine {
 
     start () {
         return ('What IP version do you want to master? Type "IPv4", "IPv6" or "both" if you want to make it really hard! ');
+    }
+
+    generateIPv4 () {
+        let iter = 4;
+        let octs = [];
+        let prefix = 0;
+        let address = '';
+
+        while (iter > 0) {
+            octs.push(this.getRandomInt(0,255));
+            iter--;
+        }
+        address = octs.join('.');
+        prefix = this.getRandomInt(1,32);
+        return address + ' / ' + prefix;
+    }
+
+    generateIPv6 () {
+      let iter = 8;
+      let hexes = [];
+      let prefix = 0;
+      let address = '';
+
+      while (iter > 0) {
+          hexes.push(this.getRandomInt(0,65535).toString(16));
+          iter--;
+      }
+      address = hexes.join(':');
+      prefix = this.getRandomInt(1,128);
+      return address + ' / ' + prefix;
+    }
+
+    getRandomInt (min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 
     invalidCMD () {
